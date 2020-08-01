1429e494dc653a924108c861cd218b44ecf2ffc2
Piotr Wenta <buraxta@gmail.com>
2020-08-01 03:08:24 +0200

    bug fixed

--- src/main/java/com/pw/willhabenParser/controllers/HouseRestController.java
+++ src/main/java/com/pw/willhabenParser/controllers/HouseRestController.java
@@ -4,12 +4,8 @@ import com.pw.willhabenParser.dao.HouseDao;
 import com.pw.willhabenParser.model.House;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,96 +18,84 @@ public class HouseRestController {
     HouseDao houseDao;
 
     @GetMapping(value = "/{limit}/{page}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<House> getPageWithLimit(@PathVariable int limit, @PathVariable int page, HttpServletRequest request) {
+    public List<House> getPageWithLimit(@PathVariable int limit, @PathVariable int page, @RequestParam Map<String, String> reqParam) {
         List<House> houseList = houseDao.getAllHouses();
 
-        Map<String, String[]> paramsMap = request.getParameterMap();
 
-        String state = paramsMap.get("state")[0];
-        if (state != null && !state.isEmpty()) {
+        if (reqParam.containsKey("state")) {
             houseList = houseList.stream()
-                    .filter(house -> house.getStateName().equalsIgnoreCase(state))
+                    .filter(house -> house.getStateName().equalsIgnoreCase(reqParam.get("state")))
                     .collect(Collectors.toList());
         }
 
-        String district = paramsMap.get("district")[0];
-        if (district != null && !district.isEmpty()) {
+        if (reqParam.containsKey("district")) {
             houseList = houseList.stream()
-                    .filter(house -> house.getDistrictName().equalsIgnoreCase(district))
+                    .filter(house -> house.getDistrictName().equalsIgnoreCase(reqParam.get("district")))
                     .collect(Collectors.toList());
         }
 
-        String type = paramsMap.get("type")[0];
-        if (type != null && !type.isEmpty()) {
+        if (reqParam.containsKey("type")) {
             houseList = houseList.stream()
-                    .filter(house -> house.getObjectType().equalsIgnoreCase(type))
+                    .filter(house -> house.getObjectType().equalsIgnoreCase(reqParam.get("type")))
                     .collect(Collectors.toList());
         }
 
-        String location = paramsMap.get("location")[0];
-        if (location != null && !location.isEmpty()) {
+        if (reqParam.containsKey("location")) {
             houseList = houseList.stream()
-                    .filter(house -> house.getLocationName().equalsIgnoreCase(location))
+                    .filter(house -> house.getLocationName().equalsIgnoreCase(reqParam.get("location")))
                     .collect(Collectors.toList());
         }
 
-        String maxPrice = paramsMap.get("maxPrice")[0];
-        if (maxPrice != null && !maxPrice.isEmpty()) {
-            double maxPriceVal = Double.parseDouble(maxPrice);
+        if (reqParam.containsKey("maxPrice")) {
+            double maxPriceVal = Double.parseDouble(reqParam.get("maxPrice"));
             houseList = houseList.stream()
                     .filter(house -> house.getPriceAsValue() != null)
                     .filter(house -> house.getPriceAsValue() <= maxPriceVal)
                     .collect(Collectors.toList());
         }
 
-        String minPrice = paramsMap.get("minPrice")[0];
-        if (minPrice != null && !minPrice.isEmpty()) {
-            double minPriceVal = Double.parseDouble(minPrice);
+        if (reqParam.containsKey("minPrice")) {
+            double minPriceVal = Double.parseDouble(reqParam.get("minPrice"));
             houseList = houseList.stream()
                     .filter(house -> house.getPriceAsValue() != null)
                     .filter(house -> house.getPriceAsValue() >= minPriceVal)
                     .collect(Collectors.toList());
         }
 
-        String size = paramsMap.get("size")[0];
-        if (size != null && !size.isEmpty()) {
-            double sizeVal = Double.parseDouble(size);
+        if (reqParam.containsKey("size")) {
+            double sizeVal = Double.parseDouble(reqParam.get("size"));
             houseList = houseList.stream()
                     .filter(house -> house.getSizeAsValue() != null)
                     .filter(house -> house.getSizeAsValue() >= sizeVal)
                     .collect(Collectors.toList());
         }
 
-        String maxSize = paramsMap.get("maxSize")[0];
-        if (maxSize != null && !maxSize.isEmpty()) {
-            double maxSizeVal = Double.parseDouble(maxSize);
+        if (reqParam.containsKey("maxSize")) {
+            double maxSizeVal = Double.parseDouble(reqParam.get("maxSize"));
             houseList = houseList.stream()
                     .filter(house -> house.getSizeAsValue() != null)
                     .filter(house -> house.getSizeAsValue() <= maxSizeVal)
                     .collect(Collectors.toList());
         }
 
-        String minArea = paramsMap.get("minArea")[0];
-        if (minArea != null && !minArea.isEmpty()) {
-            double minAreaVal = Double.parseDouble(minArea);
+        if (reqParam.containsKey("area")) {
+            double minAreaVal = Double.parseDouble(reqParam.get("area"));
             houseList = houseList.stream()
                     .filter(house -> house.getGroundAreaAsValue() != null)
                     .filter(house -> house.getGroundAreaAsValue() >= minAreaVal)
                     .collect(Collectors.toList());
         }
 
-        String rooms = paramsMap.get("rooms")[0];
-        if (rooms != null && !rooms.isEmpty()) {
-            double roomsVal = Double.parseDouble(rooms);
+        if (reqParam.containsKey("rooms")) {
+            double roomsVal = Double.parseDouble(reqParam.get("rooms"));
             houseList = houseList.stream()
                     .filter(house -> house.getRoomsAsValue() != null)
                     .filter(house -> house.getRoomsAsValue() >= roomsVal)
                     .collect(Collectors.toList());
         }
 
-        String verified = paramsMap.get("verified")[0];
-        if (verified != null && !verified.isEmpty()) {
-            boolean verifiedVal = verified.equalsIgnoreCase("true");
+        if (reqParam.containsKey("verified")) {
+            boolean verifiedVal = reqParam.get("verified").equalsIgnoreCase("true");
             houseList = houseList.stream()
                     .filter(house -> house.isVerified() == verifiedVal)
                     .collect(Collectors.toList());
