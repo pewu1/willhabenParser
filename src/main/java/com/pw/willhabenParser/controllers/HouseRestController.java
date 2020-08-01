b4b2acee9ff4202578d4bb80b48c5c13f59008d8
Piotr Wenta <buraxta@gmail.com>
2020-08-01 16:47:02 +0200

    added filters

--- src/main/java/com/pw/willhabenParser/controllers/HouseRestController.java
+++ src/main/java/com/pw/willhabenParser/controllers/HouseRestController.java
@@ -4,10 +4,7 @@ import com.pw.willhabenParser.dao.HouseDao;
 import com.pw.willhabenParser.model.House;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,33 +21,75 @@ public class HouseRestController {
         return houseDao.getAllHouses();
     }
 
-    @GetMapping(value = "/{limit}/{page}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<House> getPageWithLimit(@PathVariable int limit, @PathVariable int page) {
-        List<House> houseList = houseDao.getAllHouses();
 
+    @GetMapping(value = "/{state}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<House> getForState(@PathVariable String state, @RequestParam(required = false) String maxPrice) {
+        List<House> houseList = houseDao.getAllHouses().stream()
+                .filter(house -> house.getStateName() != null)
+                .filter(house -> house.getStateName().equalsIgnoreCase(state))
+                .collect(Collectors.toList());
+
+        if (maxPrice != null) {
+            houseList = filterForMaxPrice(houseList, maxPrice);
+        }
+
+        return houseList;
+    }
+
+    @GetMapping(value = "/{state}/{district}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<House> getForDistrict(@PathVariable String state, @PathVariable String district, @RequestParam(required = false) String maxPrice) {
+        List<House> houseList = houseDao.getAllHouses().stream()
+                .filter(house -> house.getStateName() != null)
+                .filter(house -> house.getStateName().equalsIgnoreCase(state))
+                .filter(house -> house.getDistrictName() != null)
+                .filter(house -> house.getDistrictName().equalsIgnoreCase(district))
+                .collect(Collectors.toList());
+
+        if (maxPrice != null) {
+            houseList = filterForMaxPrice(houseList, maxPrice);
+        }
+
+
+        return houseList;
+    }
+
+    @GetMapping(value = "/{state}/{district}/{location}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<House> getForLocation(@PathVariable String state, @PathVariable String district, @PathVariable String location, @RequestParam(required = false) String maxPrice) {
+        List<House> houseList = houseDao.getAllHouses().stream()
+                .filter(house -> house.getStateName() != null)
+                .filter(house -> house.getStateName().equalsIgnoreCase(state))
+                .filter(house -> house.getDistrictName() != null)
+                .filter(house -> house.getDistrictName().equalsIgnoreCase(district))
+                .filter(house -> house.getLocationName() != null)
+                .filter(house -> house.getLocationName().equalsIgnoreCase(location))
+                .collect(Collectors.toList());
+
+        if (maxPrice != null) {
+            houseList = filterForMaxPrice(houseList, maxPrice);
+        }
+
+        return houseList;
+    }
+
+    private List<House> paginateList(List<House> houseList, int limit, int page) {
         int skip = limit * (page - 1);
         return houseList.stream().skip(skip).limit(limit).collect(Collectors.toList());
     }
 
-    @GetMapping(value = "/state/{state}/{limit}/{page}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<House> getStatePageWithLimit(@PathVariable int limit, @PathVariable int page, @PathVariable String state) {
-        List<House> houseList = houseDao.getAllHouses();
+    private List<House> filterForMaxPrice(List<House> houseList, String maxPrice) {
+        if (isValidParameter(maxPrice)) {
+            double maxPriceVal = Double.parseDouble(maxPrice.replace(",", "."));
+            return houseList.stream()
+                    .filter(house -> house.getPriceAsValue() != null)
+                    .filter(house -> house.getPriceAsValue() <= maxPriceVal)
+                    .collect(Collectors.toList());
 
-        int skip = limit * (page - 1);
-        return houseList.stream()
-                .filter(house -> house.getStateName() != null)
-                .filter(house -> house.getStateName().equalsIgnoreCase(state))
-                .skip(skip).limit(limit).collect(Collectors.toList());
+        } else {
+            return houseList;
+        }
     }
 
-    @GetMapping(value = "/district/{district}/{limit}/{page}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<House> getDistrictPageWithLimit(@PathVariable int limit, @PathVariable int page, @PathVariable String district) {
-        List<House> houseList = houseDao.getAllHouses();
-
-        int skip = limit * (page - 1);
-        return houseList.stream()
-                .filter(house -> house.getDistrictName() != null)
-                .filter(house -> house.getDistrictName().equalsIgnoreCase(district))
-                .skip(skip).limit(limit).collect(Collectors.toList());
+    private boolean isValidParameter(String param) {
+        return param != null && !param.isEmpty() && !param.equalsIgnoreCase("0");
     }
 }
