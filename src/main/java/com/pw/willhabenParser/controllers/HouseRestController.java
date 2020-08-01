dc40f4bca7d3d8fc35dfeb738ee0cfa5c7d64bb4
Piotr Wenta <buraxta@gmail.com>
2020-08-01 03:28:28 +0200

    bug fixed

--- src/main/java/com/pw/willhabenParser/controllers/HouseRestController.java
+++ src/main/java/com/pw/willhabenParser/controllers/HouseRestController.java
@@ -4,10 +4,12 @@ import com.pw.willhabenParser.dao.HouseDao;
 import com.pw.willhabenParser.model.House;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -17,91 +19,38 @@ public class HouseRestController {
     @Autowired
     HouseDao houseDao;
 
+    @GetMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<House> getAll() {
+        return houseDao.getAllHouses();
+    }
+
     @GetMapping(value = "/{limit}/{page}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<House> getPageWithLimit(@PathVariable int limit, @PathVariable int page, @RequestParam Map<String, String> reqParam) {
+    public List<House> getPageWithLimit(@PathVariable int limit, @PathVariable int page) {
         List<House> houseList = houseDao.getAllHouses();
-
-
-        if (reqParam.containsKey("state")) {
-            houseList = houseList.stream()
-                    .filter(house -> house.getStateName().equalsIgnoreCase(reqParam.get("state")))
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("district")) {
-            houseList = houseList.stream()
-                    .filter(house -> house.getDistrictName().equalsIgnoreCase(reqParam.get("district")))
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("type")) {
-            houseList = houseList.stream()
-                    .filter(house -> house.getObjectType().equalsIgnoreCase(reqParam.get("type")))
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("location")) {
-            houseList = houseList.stream()
-                    .filter(house -> house.getLocationName().equalsIgnoreCase(reqParam.get("location")))
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("maxPrice")) {
-            double maxPriceVal = Double.parseDouble(reqParam.get("maxPrice"));
-            houseList = houseList.stream()
-                    .filter(house -> house.getPriceAsValue() != null)
-                    .filter(house -> house.getPriceAsValue() <= maxPriceVal)
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("minPrice")) {
-            double minPriceVal = Double.parseDouble(reqParam.get("minPrice"));
-            houseList = houseList.stream()
-                    .filter(house -> house.getPriceAsValue() != null)
-                    .filter(house -> house.getPriceAsValue() >= minPriceVal)
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("size")) {
-            double sizeVal = Double.parseDouble(reqParam.get("size"));
-            houseList = houseList.stream()
-                    .filter(house -> house.getSizeAsValue() != null)
-                    .filter(house -> house.getSizeAsValue() >= sizeVal)
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("maxSize")) {
-            double maxSizeVal = Double.parseDouble(reqParam.get("maxSize"));
-            houseList = houseList.stream()
-                    .filter(house -> house.getSizeAsValue() != null)
-                    .filter(house -> house.getSizeAsValue() <= maxSizeVal)
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("area")) {
-            double minAreaVal = Double.parseDouble(reqParam.get("area"));
-            houseList = houseList.stream()
-                    .filter(house -> house.getGroundAreaAsValue() != null)
-                    .filter(house -> house.getGroundAreaAsValue() >= minAreaVal)
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("rooms")) {
-            double roomsVal = Double.parseDouble(reqParam.get("rooms"));
-            houseList = houseList.stream()
-                    .filter(house -> house.getRoomsAsValue() != null)
-                    .filter(house -> house.getRoomsAsValue() >= roomsVal)
-                    .collect(Collectors.toList());
-        }
-
-        if (reqParam.containsKey("verified")) {
-            boolean verifiedVal = reqParam.get("verified").equalsIgnoreCase("true");
-            houseList = houseList.stream()
-                    .filter(house -> house.isVerified() == verifiedVal)
-                    .collect(Collectors.toList());
-        }
 
         int skip = limit * (page - 1);
         return houseList.stream().skip(skip).limit(limit).collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/state/{state}/{limit}/{page}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<House> getStatePageWithLimit(@PathVariable int limit, @PathVariable int page, @PathVariable String state) {
+        List<House> houseList = houseDao.getAllHouses();
+
+        int skip = limit * (page - 1);
+        return houseList.stream()
+                .filter(house -> house.getStateName() != null)
+                .filter(house -> house.getStateName().equalsIgnoreCase(state))
+                .skip(skip).limit(limit).collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/district/{district}/{limit}/{page}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<House> getDistrictPageWithLimit(@PathVariable int limit, @PathVariable int page, @PathVariable String district) {
+        List<House> houseList = houseDao.getAllHouses();
+
+        int skip = limit * (page - 1);
+        return houseList.stream()
+                .filter(house -> house.getDistrictName() != null)
+                .filter(house -> house.getDistrictName().equalsIgnoreCase(district))
+                .skip(skip).limit(limit).collect(Collectors.toList());
     }
 }
