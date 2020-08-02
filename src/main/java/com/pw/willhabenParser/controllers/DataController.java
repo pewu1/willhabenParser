f4b616b3c9517c5f1c7ffd86153585e7cf8353fe
Piotr Wenta <buraxta@gmail.com>
2020-08-02 14:53:46 +0200

    added react

--- src/main/java/com/pw/willhabenParser/controllers/DataController.java
+++ src/main/java/com/pw/willhabenParser/controllers/DataController.java
@@ -1,6 +1,5 @@
 package com.pw.willhabenParser.controllers;
 
-import com.pw.willhabenParser.service.HTMLRenderer;
 import com.pw.willhabenParser.service.ScheduledDataFetchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,25 +10,7 @@ public class DataController {
 
     @Autowired
     ScheduledDataFetchService scheduledDataFetchService;
-
-    @Autowired
-    HTMLRenderer htmlRenderer;
-
-    @GetMapping("/newest")
-    public String newestHouses() {
-        return htmlRenderer.getHTMLNewestHouses();
-    }
-
-    @GetMapping("/belowavg")
-    public String belowAvgHouses() {
-        return htmlRenderer.getHTMLBelowAvg();
-    }
-
-    @GetMapping("/oo")
-    public String newestUpperAustria() {
-        return htmlRenderer.getHTMLNewestUpperAustria();
-    }
-
+    
     @GetMapping("/fetchdata")
     public String keepAlive() {
         try {
--- src/main/java/com/pw/willhabenParser/controllers/HouseRestController.java
+++ src/main/java/com/pw/willhabenParser/controllers/HouseRestController.java
@@ -14,6 +14,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/houses")
 public class HouseRestController {
