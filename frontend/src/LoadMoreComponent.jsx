62d88477ab23abce88936739052fcdc1300b5772
Piotr Wenta <buraxta@gmail.com>
2020-08-15 19:33:26 +0200

    added pagination and remove function

---
+++ frontend/src/LoadMoreComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { RightCircleTwoTone } from "@ant-design/icons";
+
+function loadMoreHouses(props) {
+  return <div>LOAD NEXT </div>;
+}
+
+export default loadMoreHouses;
